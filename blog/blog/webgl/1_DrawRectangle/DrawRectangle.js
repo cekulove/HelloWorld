1deaecb8e1a779736d349272bc7cdccf629ce241
cekulove <1046782978@qq.com>
2020-02-18 14:31:55 +0800

    update

---
+++ blog/blog/webgl/1_DrawRectangle/DrawRectangle.js
@@ -0,0 +1,15 @@
+function main(){
+    //  Retrieve <canvas> element
+    let canvas=document.getElementById("example");
+    if (!canvas){
+        console.log("Failed to retrieve the <canvas> element.")
+        return;
+    }
+
+    //  Get the rendering context for 2DCG
+    let ctx=canvas.getContext("2d");
+
+    //  Draw a blue rectangle
+    ctx.fillStyle="rgba(0, 0, 255, 1.0)"    //  Set color to blue
+    ctx.fillRect(120, 10, 150, 150);        //  Fill a rectangle with the color
+}
